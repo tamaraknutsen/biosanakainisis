193a4981302797b6dbcf5722a585c6478691e3f6
tamaraknutsen <knutsen@gmail.com>
2014-12-02 19:54:55 -0800

    Tweaked main picture and names

--- routes.js
+++ routes.js
@@ -8,3 +8,23 @@ Router.route('/',
             {name: 'index'},
             function () { this.render('index') }
             );
+Router.route('/location',
+            {name: 'location'},
+            function () { this.render('location') }
+            );
+Router.route('/schedule',
+            {name: 'schedule'},
+            function () { this.render('schedule') }
+            );
+Router.route('/rsvp',
+            {name: 'rsvp'},
+            function () { this.render('rsvp') }
+            );
+Router.route('/registry',
+            {name: 'registry'},
+            function () { this.render('registry') }
+            );
+Router.route('/lodging',
+            {name: 'lodging'},
+            function () { this.render('lodging') }
+            );
